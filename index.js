a1321d0f48d129e63ea87617f0e5afdbce758c32
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-04-30 14:28:58 -0400

    completed

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+
+  } else if (ride> 400 && ride <= 2000) {
+    return 'That will be twenty bucks.'
+
+  } else if (ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+
+  } else if (ride >2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
   // Write your code here!
+  let r
+   x==='NYC'?(r = 'Ok, sounds good.'):(r='No go.')
+  return r
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(y){
   // Write your code here!
+  switch(y) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+
+  }
 }
